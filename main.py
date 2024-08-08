362f20f5d53739ea6251ab2b666bb9fc90cf81c8
Acidicts <111660544+Acidicts@users.noreply.github.com>
2024-08-08 15:09:18 +0100

    Platformer PT 2

--- main.py
+++ main.py
@@ -18,8 +18,40 @@ pygame.display.set_caption("Platformer")
 surf = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
+def flip(sprite):
+    return [pygame.transform.flip(frame, True, False) for frame in sprite]
+
+
+def load_spritesheet(path, width, height, direction=False):
+    full_path = path
+    images = [f for f in listdir(full_path) if isfile(join(full_path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(full_path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surf = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect((i * width, 0, width, height))
+            surf.blit(sprite_sheet, (0, 0), rect)
+
+            sprites.append(pygame.transform.scale2x(surf))
+
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+
+    return all_sprites
+
+
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
+    GRAVITY = 1
+    SPRITES = load_spritesheet("assets/MainCharacters/NinjaFrog", 32, 32, True)
 
     def __init__(self, x, y, width, height):
         super().__init__()
@@ -30,6 +62,11 @@ class Player(pygame.sprite.Sprite):
         self.mask = None
 
         self.direction = "left"
+
+        self.frames_index = 0
+        self.fall_count = 0
+
+        self.status = "idle"
 
     def move(self, dx, dy):
         self.rect.x += dx
@@ -45,12 +82,34 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, fps):
         win = pygame.display.get_surface()
+
+        self.get_state()
+        self.animate()
+
+        self.rect = self.image.get_rect(topleft=self.rect.topleft)
+        self.mask = pygame.mask.from_surface(self.image)
+
+        self.fall_count += 1
+        self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+
         self.input()
         self.move(self.x_vel, self.y_vel)
         self.draw(win)
 
+    def animate(self):
+        self.frames_index += 0.5
+        self.image = self.SPRITES[self.status][(int(self.frames_index) % len(self.SPRITES[self.status]))]
+
     def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, self.rect)
+        win.blit(self.image, self.rect.topleft)
+
+    def get_state(self):
+        self.status = "idle" + "_" + self.direction
+        if self.x_vel != 0:
+            self.status = "run" + "_" + self.direction
+        if self.y_vel > 0:
+            self.status = "fall" + "_" + self.direction
+
 
 
 def get_background(name):
